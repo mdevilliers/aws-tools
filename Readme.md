9b082f70289c1341927be5487077ceb5885bc9b8
Mark deVilliers <markdevilliers@gmail.com>
2016-01-26 14:29:21 +0000

    added help messages to describe the environmental varaibles to use

--- Readme.md
+++ Readme.md
@@ -12,8 +12,9 @@ pip install -r requirements.txt
 ```
 
 ```
-usage: aws_reporter.py [-h] -aws-access-key AWS_ACCESS_KEY -aws-secret-key
-                       AWS_SECRET_KEY [--email-password EMAIL_PASSWORD]
+usage: aws_reporter.py [-h] [-aws-access-key AWS_ACCESS_KEY]
+                       [-aws-secret-key AWS_SECRET_KEY]
+                       [--email-password EMAIL_PASSWORD]
                        [--email-from EMAIL_FROM] [--email-to EMAIL_TO]
                        [--reports REPORTS]
 
@@ -22,14 +23,23 @@ Collates a report of running AWS instances
 optional arguments:
   -h, --help            show this help message and exit
   -aws-access-key AWS_ACCESS_KEY
+                        AWS Access Key (can also be specified using
+                        AWS_ACCESS_KEY environment variable)
   -aws-secret-key AWS_SECRET_KEY
+                        AWS Secret Key (can also be specified using
+                        AWS_SECRET_KEY environment variable)
   --email-password EMAIL_PASSWORD
+                        Gmail Email password (can also be specified using
+                        EMAIL_PASSWORD environment variable)
   --email-from EMAIL_FROM
-  --email-to EMAIL_TO   Comma seperated list of email recipients
-  --reports REPORTS     Comma seperated list frawn from the following -
-                        Console, Email
+                        From email address (can also be specified using
+                        EMAIL_FROM environment variable)
+  --email-to EMAIL_TO   Comma seperated list of email recipients (can also be
+                        specified using EMAIL_TO environment variable)
+  --reports REPORTS     Comma seperated list from from the following -
+                        Console, Email (can also be specified using REPORTS
+                        environment variable)
 ```
-
 
 To report to the console
 
@@ -41,4 +51,10 @@ To send an email (only gmail supported) plus report to the console and email
 
 ```
 python aws_reporter.py --aws-secret-key {REDACTED} --aws-access-key REDACTED --email-password {REDACTED} --email-from abc@abc.com --email-to one@abc.com,two@abc.com --reports Email,Console
+```
+
+Runing via docker
+
+```
+docker run --env AWS_ACCESS_KEY={REDACTED} --env AWS_SECRET_KEY={REDACTED} {IMAGE_NAME}
 ```
--- aws_reporter.py
+++ aws_reporter.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Action
 
 from aws.price import PriceNotFoundError, AWSPricingStore
 from aws.instance import AWSRunningInstances, AWSInstance
@@ -12,13 +12,14 @@ from datetime import datetime
 import json 
 import logging
 import math
+import os
 import sys
 
 regions = ['us-east-1','us-west-1','us-west-2','eu-west-1','sa-east-1',
             'ap-southeast-1','ap-southeast-2','ap-northeast-1']
 
 class Whitelist(object):
-
+    """Maintains a white list of instances which ren't included in the reports"""
     def __init__(self, whitelistfile = 'data.whitelist.json'):
         # REVIEW : is it okay if this just throws
         with open(whitelistfile) as data_file:
@@ -28,30 +29,55 @@ class Whitelist(object):
     def ok(self, identifier) :
         return not identifier in self._data
 
+# http://stackoverflow.com/questions/10551117/setting-options-from-environment-variables-when-using-argparse
+class EnvDefault(Action):
+    """Will inspect the environment for a variable before looking for a command line value"""
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required, 
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
 def main():
 
     parser = ArgumentParser(
         'aws_reporter.py', description="Collates a report of running AWS instances"
     )
     parser.add_argument(
-        '-aws-access-key', required=True
+        '-aws-access-key', required=True,
+        action=EnvDefault, envvar='AWS_ACCESS_KEY',
+        help='AWS Access Key (can also be specified using AWS_ACCESS_KEY environment variable)' 
     )
     parser.add_argument(
-        '-aws-secret-key', required=True
+        '-aws-secret-key', required=True, 
+        action=EnvDefault, envvar='AWS_SECRET_KEY',
+        help='AWS Secret Key (can also be specified using AWS_SECRET_KEY environment variable)'
     )
     parser.add_argument(
-        '--email-password', default=None
+        '--email-password', default=None, required=False,
+        action=EnvDefault, envvar='EMAIL_PASSWORD',
+        help='Gmail Email password (can also be specified using EMAIL_PASSWORD environment variable)'
     )
     parser.add_argument(
-        '--email-from', default=None
+        '--email-from', default=None, required=False,
+        action=EnvDefault, envvar='EMAIL_FROM',
+        help='From email address (can also be specified using EMAIL_FROM environment variable)'
     )
     parser.add_argument(
-        '--email-to', default=None,
-        help="Comma seperated list of email recipients"
+        '--email-to', default=None, required=False,
+        action=EnvDefault, envvar='EMAIL_TO',
+        help='Comma seperated list of email recipients (can also be specified using EMAIL_TO environment variable)'
     )
     parser.add_argument(
-        '--reports', default="Console", 
-        help="Comma seperated list frawn from the following - Console, Email"
+        '--reports', default='Console', required=False,
+        action=EnvDefault, envvar='REPORTS',  
+        help='Comma seperated list from from the following - Console, Email (can also be specified using REPORTS environment variable)'
     )
 
     opts = parser.parse_args()
@@ -67,7 +93,7 @@ def main():
 
     # format and send the reports
     _output_reports(instances, opts.reports.split(","), opts)
-   
+
 def _execute_report(aws_access_key, aws_secret_key):
 
     now = datetime.utcnow()
