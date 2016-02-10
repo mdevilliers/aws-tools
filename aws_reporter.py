67fa4427eff2a8bebd29eb3c880c241cab73fcd0
Mark deVilliers <markdevilliers@gmail.com>
2016-02-10 08:14:16 +0000

    add in eu-central to the list of regions requiring reporting.

--- aws_reporter.py
+++ aws_reporter.py
@@ -16,7 +16,7 @@ import os
 import sys
 
 regions = ['us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'sa-east-1',
-            'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1']
+            'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'eu-central-1']
 
 def main():
 
